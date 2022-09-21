836a08c5f102802241b18c68cf5f46c1b69d6e15
vivizuma <vitomanz@hotmail.co.uk>
2022-09-21 19:01:27 +0100

    Random PC choice added, draw function

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
+let pcChoice = "";
+let userChoice="";
 function getComputerChoice() {
     min = Math.ceil(4);
     max = Math.floor(1);
     let choice = Math.floor(Math.random() * (max-min) + min);
     console.log(choice);
-    if (choice === 1) {return "rock"}
-    else if (choice === 2) {return "paper"}
-    else {return "scissors"}
+    if (choice === 1) { return pcChoice="rock"}
+    else if (choice === 2) { return pcChoice= "paper"}
+    else {return pcChoice="scissors"}
 }
 
 
-let compChoice = getComputerChoice() 
-console.log(compChoice)
 
+
+
+function decision() {let userChoice = prompt("Rock, paper or scissors?");
+console.log(userChoice)
+}
+
+function playGame(userChoice, pcChoice) {
+    if (userChoice == pcChoice) {
+        console.log("Draw!");
+    } else if ((userChoice == "rock") && pcChoice == "paper")
+    { console.log("You lose");
+    } else if ((userChoice == "rock") && (pcChoice == "scissors")) {
+        console.log("You win!");
+    } else {console.log("undefff")}
+}
+ 
+userChoice = decision()
+pcChoice = getComputerChoice()
+console.log(`"Your opponent chose ${pcChoice}"`)
+
+playGame(userChoice, pcChoice)
 // playGame()
 // win if :
 // playerChoice is rock vs scissors
